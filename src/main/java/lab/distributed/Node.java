ab185fe9edfdc85ba30e26d2d526050053363da6
robrecht <robrechtdaems@msn.com>
2016-10-25 11:46:45 +0200

    begin Node

--- src/main/java/lab/distributed/Node.java
+++ src/main/java/lab/distributed/Node.java
@@ -1,10 +1,23 @@
 package lab.distributed;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by Ivan on 24/10/2016.
  */
 public class Node {
 
     private String name;
+    private String location;
+
+    public Node(String name){
+        this.name=name;
+        try {
+            location=InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
