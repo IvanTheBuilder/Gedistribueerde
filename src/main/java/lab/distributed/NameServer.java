6414be4770a2e8da53ad61e437e1bcd4561ee52a
KurkDroog <robbe.elsas@gmail.com>
2016-11-08 10:45:18 +0100

    update

--- src/main/java/lab/distributed/NameServer.java
+++ src/main/java/lab/distributed/NameServer.java
@@ -148,6 +148,7 @@ public class NameServer implements NameServerInterface {
                             dataOutputStream.writeUTF("size "+nodeMap.size());
                         else
                             dataOutputStream.writeUTF("duplicate ");
+
                         dataOutputStream.close();
                     }
                 } catch (IOException e) {
