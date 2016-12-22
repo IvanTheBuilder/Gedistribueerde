df31c4f939fe7f7c3724afb8c22259dc40a342c9
KurkDroog <robbe.elsas@gmail.com>
2016-12-22 11:47:12 +0100

    file agent hotpatch

--- src/main/java/lab/distributed/FileAgent.java
+++ src/main/java/lab/distributed/FileAgent.java
@@ -83,10 +83,10 @@ public class FileAgent implements AgentInterface, Serializable {
                  */
                 int ownerOfLock = lockedFilesMap.putIfAbsent(entry.getKey(), entry.getValue() ? currentNode.getMyHash() : -1);
                 //boolean currentIsFirst = currentNode.getMyHash() < currentNode.getPreviousNode();
-               // boolean lastNodeIsGone = currentIsFirst && ownerOfLock > currentNode.getPreviousNode();
+                //boolean lastNodeIsGone = currentIsFirst && ownerOfLock > currentNode.getPreviousNode();
                 boolean isReplaced = false;
                 //if ((lastNodeIsGone || (currentNode.getMyHash() > ownerOfLock)) && (ownerOfLock != -1)) {
-                if (!currentNode.nodeIsPresent(ownerOfLock)) {
+                if (!currentNode.nodeIsPresent(ownerOfLock) && (ownerOfLock != -1)) {
                     //TODO: print statements verwijderen na testen
                     System.out.println("The file agent noticed node with hash: "+ownerOfLock+" has exited or failed");
                     System.out.println("The lock of "+ownerOfLock+" on "+entry.getKey()+" has been released");
